4193a900ac17c95aaf93e4fea2b477f881f1a7c6
RoysManfo <manforoys@gmail.com>
2023-02-02 00:18:48 +0100

    Move component

--- src/pages/Minigiochi.tsx
+++ src/pages/Minigiochi.tsx
@@ -1,47 +1,30 @@
 import React from "react";
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer";
+import LinkList from "../components/LinkList";
+import '../css/categories/categories.css'
 
-export default Minigiochi;
 
 
-class LinkList{
-    private links: string[] = [];
-
-    // Pass in a list of objects describing <link> elements where each link is in format
-    /*
+const LINKS: {rel: string; type: string; href: string;}[] = [
     {
-        "rel": "...",
-        "type": "...",
-        "href": "...",
-    }
-    */
-    
-    constructor(links: string[]) {
+        "rel": "stylesheet",
+        "type": "text/css",
+        "href": "../css/minigiochi/minigiochi.css"
+    },
+]
 
-    }
-
-}
-
-
-function addLinks(){
-    const links = document.querySelector('head') as HTMLElement;
-    let l = document.createElement('link');
-    l.rel = 'stylesheet';
-    l.type = 'text/css';
-    l.href = "s.css"
-
-    links.appendChild(l);
-}
-
-function Minigiochi() {
+export default function Minigiochi() {
     return(
         <>
+            <LinkList props = {LINKS} />
             <Navbar />
             <main>
                 <p className="font-6 txt-center" style={{marginTop: "10px"}}>Minigiochi</p>
-
             </main>
+
+
+
         </>
     )    
 }
