de3d7169f77e5973adbfc5bbaed54fedb547affd
RoysManfo <manforoys@gmail.com>
2023-03-05 16:31:52 +0100

    Removed unused component

--- src/pages/Minigiochi.tsx
+++ src/pages/Minigiochi.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../components/Navbar"
-import Footer from "../components/Footer";
 import Card from "../components/Card"
 
 import "../css/categories/categories.css"
