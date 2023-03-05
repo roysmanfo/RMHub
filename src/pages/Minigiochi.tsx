ef37727c115c9fc802baee15f46256354c3c85d7
RoysManfo <manforoys@gmail.com>
2023-03-05 14:03:26 +0100

    Solved problem with Card elements

--- src/pages/Minigiochi.tsx
+++ src/pages/Minigiochi.tsx
@@ -6,7 +6,17 @@ import Card from "../components/Card"
 import "../css/categories/categories.css"
 import "../css/minigiochi/minigiochi.css"
 
-import pic from "../img/generic/bg-main_2.jpg"
+import street from "../img/generic/bg-main_2.jpg"
+
+
+const CARDS = [
+    {
+        "caption": "Sasso Carta Forbice",
+        "img": street,
+        "link": "sasso-carta-forbice/",
+        "id": "sasso-carta-forbice"
+    }
+]
 
 export default function Minigiochi() {
     return(
@@ -15,7 +25,7 @@ export default function Minigiochi() {
             <main style={{ flexDirection : "column" }}>
                 <p className="font-6 txt-center" style={{marginTop: "10px", width: "100%"}}>Minigiochi</p>
                 <section style={{ display: "flex", justifyContent: "space-around" }}>
-                    <Card caption="Sasso Carta Forbice" img={pic} link="sasso-carta-forbice/" id="sasso-carta-forbice" />
+                    <Card caption={CARDS[0].caption} img={CARDS[0].img} link={CARDS[0].link} id={CARDS[0].id} />
                 </section>
             </main>
 
