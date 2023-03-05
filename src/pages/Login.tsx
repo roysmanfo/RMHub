67fee441d8defc4348d39a3b945f6e941d54c1de
RoysManfo <manforoys@gmail.com>
2023-03-05 19:20:04 +0100

    Implement Sign up

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,57 +1,68 @@
-import React from 'react';
-import app from '../conf/firebase';
-import "../css/login/login.css"
+import React, { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import "../css/login/login.css";
+import ENV from '../env';
 
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
 
+    // Create a single supabase client for interacting with your database
+    const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SUPABASE_KEY);
 
-export default class Login extends React.Component{
+    async function signUp() {
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: pass,
+        })
 
-    login(){
-
-    }
-
-    signUp(){
-
-
-    }
-
-    render(): React.ReactNode {
-        return(
-            <>
-                <main className='login-main'>
-                    <form action="" method="post">
-                        <h1 style={{marginBottom: "4rem", color: "#999"}}>Login</h1>
-                        <input name='email' type="email" id="email" placeholder='Email'/>
-                        <input name='password' type="password" id="password" placeholder='Password'/>
-                        <div className="button-wrapper">
-                            <button onClick={this.login}>Login</button>
-                            <button onClick={this.signUp}>Sign Up</button>
-                        </div>
-                    </form>
-                </main>
-            </>
-        )
-    }
-}
-
-class Auth extends React.Component{
-    constructor(props: any){
-        super(props);
-        
-        this.state = {
-            user: null,
+        if (error) {
+            console.log(error);
+            return
+        }
+        console.log(data)
+        const { data: userData, error: userError } = await supabase.from('users').insert({ email: email }).single();
+        if (userError) {
+            console.log(userError);
+            return
         }
 
-        this.authListener = this.authListener.bind(this);
+        console.log(userData);
     }
 
-    componentDidMount(): void {
-        this.authListener();  
+    async function login() {
+        let { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: pass,
+        })
+
+        if (error) {
+            console.log(error);
+            return
+        }
+        console.log(data)
+        const { data: userData, error: userError } = await supabase.from('users').insert({ email: email }).single();
+        if (userError) {
+            console.log(userError);
+            return
+        }
+
+        console.log(userData);
     }
 
-    authListener(){
-        // app.auth().onAuthStateChanged((user) => {
-            
-        // });
-    }
+    return (
+        <>
+            <main className='login-main'>
+                <form action="" method="post">
+                    <h1 style={{ marginBottom: "4rem", color: "#999" }}>Login</h1>
+                    <input name='email' type="email" id="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input name='password' type="password" id="password" placeholder='Password' value={pass} onChange={(e) => setPass(e.target.value)} />
+                    <div className="button-wrapper">
+                        <button onClick={login} type="button">Login</button>
+                        <button onClick={signUp}>Sign Up</button>
+                    </div>
+                </form>
+            </main>
+        </>
+    )
 }
--- src/pages/Minigiochi.tsx
+++ src/pages/Minigiochi.tsx
@@ -7,6 +7,15 @@ import "../css/minigiochi/minigiochi.css"
 
 import street from "../img/generic/bg-main_2.jpg"
 
+import ENV from '../env';
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SUPABASE_KEY);
+async function usr(){
+    const { data: { user } } = await supabase.auth.getUser()
+    console.log(user);
+}
+
+
 
 const CARDS = [
     {
@@ -17,11 +26,13 @@ const CARDS = [
     }
 ]
 
+
 export default function Minigiochi() {
     return(
         <>
             <Navbar />
             <main style={{ flexDirection : "column" }}>
+                <button onClick={usr}> s</button>
                 <p className="font-6 txt-center" style={{marginTop: "10px", width: "100%"}}>Minigiochi</p>
                 <section style={{ display: "flex", justifyContent: "space-around" }}>
                     <Card caption={CARDS[0].caption} img={CARDS[0].img} link={CARDS[0].link} id={CARDS[0].id} />
