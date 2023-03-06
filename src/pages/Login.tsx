6e65914bb128a466f45c11048f3f90508b442723
RoysManfo <manforoys@gmail.com>
2023-03-06 23:58:29 +0100

    Partialy solved problem with signup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
+import { useHistory } from 'react-router-dom'; // import useHistory
 import "../css/login/login.css";
 import ENV from '../env';
 
@@ -9,6 +10,9 @@ export default function Login() {
 
     // Create a single supabase client for interacting with your database
     const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SUPABASE_KEY);
+
+     // get history object
+     const history = useHistory();
 
     async function signUp() {
         const { data, error } = await supabase.auth.signUp({
@@ -24,6 +28,7 @@ export default function Login() {
         const { data: userData, error: userError } = await supabase.from('users').insert({ email: email }).single();
         if (userError) {
             console.log(userError);
+            window.location.href = window.location.href.replace('login', 'home');
             return
         }
 
@@ -40,25 +45,25 @@ export default function Login() {
             console.log(error);
             return
         }
-        console.log(data)
+        console.log(data.user);
         const { data: userData, error: userError } = await supabase.from('users').insert({ email: email }).single();
         if (userError) {
             console.log(userError);
             return
         }
-
         console.log(userData);
+        window.location.href = window.location.href.replace('login', 'home');
     }
 
     return (
         <>
             <main className='login-main'>
-                <form action="" method="post">
+                <form action='https://roswbhnqbfckiuczsnrh.supabase.co/auth/v1' method="post">
                     <h1 style={{ marginBottom: "4rem", color: "#999" }}>Login</h1>
                     <input name='email' type="email" id="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
                     <input name='password' type="password" id="password" placeholder='Password' value={pass} onChange={(e) => setPass(e.target.value)} />
                     <div className="button-wrapper">
-                        <button onClick={login} type="button">Login</button>
+                        <button onClick={login}>Login</button>
                         <button onClick={signUp}>Sign Up</button>
                     </div>
                 </form>
