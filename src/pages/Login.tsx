64064970070de174d07c08c43aeacfa7b6f8afdc
RoysManfo <manforoys@gmail.com>
2023-03-07 22:31:41 +0100

    New icons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import { useHistory } from 'react-router-dom'; // import useHistory
+import { useNavigate } from 'react-router-dom';
 import "../css/login/login.css";
 import ENV from '../env';
 
@@ -11,8 +11,8 @@ export default function Login() {
     // Create a single supabase client for interacting with your database
     const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SUPABASE_KEY);
 
-     // get history object
-     const history = useHistory();
+    // get navigate object
+    const navigate = useNavigate();
 
     async function signUp() {
         const { data, error } = await supabase.auth.signUp({
@@ -28,11 +28,12 @@ export default function Login() {
         const { data: userData, error: userError } = await supabase.from('users').insert({ email: email }).single();
         if (userError) {
             console.log(userError);
-            window.location.href = window.location.href.replace('login', 'home');
             return
         }
 
         console.log(userData);
+        // redirect to profile page
+        navigate('/profile');
     }
 
     async function login() {
@@ -52,13 +53,14 @@ export default function Login() {
             return
         }
         console.log(userData);
-        window.location.href = window.location.href.replace('login', 'home');
+        // redirect to profile page
+        navigate('/profile');
     }
 
     return (
         <>
             <main className='login-main'>
-                <form action='https://roswbhnqbfckiuczsnrh.supabase.co/auth/v1' method="post">
+                <form method="post">
                     <h1 style={{ marginBottom: "4rem", color: "#999" }}>Login</h1>
                     <input name='email' type="email" id="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
                     <input name='password' type="password" id="password" placeholder='Password' value={pass} onChange={(e) => setPass(e.target.value)} />
