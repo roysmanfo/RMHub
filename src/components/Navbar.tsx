a394538ebafb95f8e346205c315e21ab1d422356
RoysManfo <manforoys@gmail.com>
2023-03-08 23:54:17 +0100

    Working on the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from "../img/logos/png/white-no-background.png";
+import { createClient } from '@supabase/supabase-js';
+import ENV from '../env';
 
-
+const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SUPABASE_KEY);
 export default function Navbar(props: any) {
+
     if (props.index) {
         return (
-            <nav style={{ background: "transparent", opacity: 0, animation: "light 1s ease 4s forwards"}}>
-                <a href="/" className="logo" title="Torna alla pagina principale"><img src={ logo } alt="Logo" style={{ width: "4rem" }}/></a>
+            <nav style={{ background: "transparent", opacity: 0, animation: "light 1s ease 4s forwards" }}>
+                <a href="/" className="logo" title="Torna alla pagina principale"><img src={logo} alt="Logo" style={{ width: "4rem" }} /></a>
                 <ul className="navList">
                     <a href="/home" rel="noopener noreferrer">
                         <li>Home</li>
@@ -21,13 +24,13 @@ export default function Navbar(props: any) {
                         <li>Info</li>
                     </a>
                 </ul>
-                <a href="/login"><button className="mainLoginButton" style={{ background: "#4443" }}>Login</button></a>
+                <LoginButton index />
             </nav>
         );
-    }else{
+    } else {
         return (
             <nav>
-                <a href="/" className="logo" title="Torna alla pagina principale"><img src={ logo } alt="Logo" style={{ width: "4rem" }} /></a>
+                <a href="/" className="logo" title="Torna alla pagina principale"><img src={logo} alt="Logo" style={{ width: "4rem" }} /></a>
                 <ul className="navList">
                     <a href="/home" rel="noopener noreferrer">
                         <li>Home</li>
@@ -42,9 +45,29 @@ export default function Navbar(props: any) {
                         <li>Info</li>
                     </a>
                 </ul>
-                <a href="/login"><button className="mainLoginButton">Login</button></a>
+                <LoginButton />
             </nav>
         );
     }
 }
 
+function LoginButton(props: any) {
+    // Get the currently authenticated user
+    function getUser() {
+        const user  = supabase.auth.getUser();
+        return user;
+    }
+    const user = getUser();
+
+    if (user !== null) {
+        if (props.index) {
+            return (<a href="/login"><button className="mainLoginButton" style={{ background: "#4443" }}>Login</button></a>)
+        } else {
+            return (<a href="/login"><button className="mainLoginButton">Login</button></a>);
+        }
+    } else {
+        return (
+            <a href="/profile"><button className="mainLoginButton">Profile</button></a>
+        )
+    }
+}
