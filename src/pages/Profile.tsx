7db2720fe62ba7b5fb0846b934e64b4ad4694703
RoysManfo <manforoys@gmail.com>
2023-03-09 23:21:12 +0100

    Display some temporary info

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import { createClient } from '@supabase/supabase-js';
 import ENV from '../env';
@@ -8,32 +8,36 @@ import "../css/profile/profile.css";
 export default function Profile() {
     // Create a single supabase client for interacting with your database
     const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SUPABASE_KEY);
+    const [user, setUser] = useState<any>();
 
-    // Get the currently authenticated user
-    async function getUser() {
-        const { data: { user } } = await supabase.auth.getUser();
-        if (user) {
-            console.log('User is logged in');
-            console.log('Email:', user.email);
-            console.log('User ID:', user.id);
-        } else {
-            console.log('No user is logged in');
-            console.log(user)
+    useEffect(() => {
+        async function fetchUser() {
+            const { data: user, error } = await supabase.auth.getUser();
+            if (error) {
+                console.log('An error occurred while fetching user data:', error);
+            } else {
+                setUser(user.user);
+            }
         }
-        return user;
-    }
-    const user = getUser();
-    console.log(user);
+        fetchUser();
+    });
+
     return (
         <>
             <Navbar />
             <main className='column x-center'>
                 <h1>Profile</h1>
+                <div>
+                    {user && <p><b>ID:</b> {user.id}</p>}
+                    {user && <p><b>Email:</b> {user.email}</p>}
+                    {user && <p><b>Date of creation:</b> {user.created_at}</p>}
+                    {user && <p><b>Role:</b> {user.role}</p>}
+                </div>
                 <button className="mainLoginButton" onClick={logout}>Logout</button>
-                {/* <p>Email: { user }</p> */}
             </main>
         </>
-    )
+    );
+
     async function logout() {
         // check if user is authenticated
         const session = supabase.auth.getSession();
