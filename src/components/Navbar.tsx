d322cbe4f7aa3213b7d9515e0270353d14c206cf
RoysManfo <manforoys@gmail.com>
2023-03-09 22:26:37 +0100

    modified navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,73 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from "../img/logos/png/white-no-background.png";
 import { createClient } from '@supabase/supabase-js';
 import ENV from '../env';
 
 const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SUPABASE_KEY);
+
 export default function Navbar(props: any) {
-
-    if (props.index) {
-        return (
-            <nav style={{ background: "transparent", opacity: 0, animation: "light 1s ease 4s forwards" }}>
-                <a href="/" className="logo" title="Torna alla pagina principale"><img src={logo} alt="Logo" style={{ width: "4rem" }} /></a>
-                <ul className="navList">
-                    <a href="/home" rel="noopener noreferrer">
-                        <li>Home</li>
-                    </a>
-                    <a href="/categories">
-                        <li>Categorie</li>
-                    </a>
-                    <a href="/unavaiable" rel="noopener noreferrer">
-                        <li>Docs</li>
-                    </a>
-                    <a href="/info" rel="noopener noreferrer">
-                        <li>Info</li>
-                    </a>
-                </ul>
-                <LoginButton index />
-            </nav>
-        );
-    } else {
-        return (
-            <nav>
-                <a href="/" className="logo" title="Torna alla pagina principale"><img src={logo} alt="Logo" style={{ width: "4rem" }} /></a>
-                <ul className="navList">
-                    <a href="/home" rel="noopener noreferrer">
-                        <li>Home</li>
-                    </a>
-                    <a href="/categories">
-                        <li>Categorie</li>
-                    </a>
-                    <a href="/unavaiable" rel="noopener noreferrer">
-                        <li>Docs</li>
-                    </a>
-                    <a href="/info" rel="noopener noreferrer">
-                        <li>Info</li>
-                    </a>
-                </ul>
-                <LoginButton />
-            </nav>
-        );
-    }
+    return (
+        <nav>
+            <a href="/" className="logo" title="Torna alla pagina principale"><img src={logo} alt="Logo" style={{ width: "4rem" }} /></a>
+            <ul className="navList">
+                <a href="/home" rel="noopener noreferrer">
+                    <li>Home</li>
+                </a>
+                <a href="/categories">
+                    <li>Categorie</li>
+                </a>
+                <a href="/unavaiable" rel="noopener noreferrer">
+                    <li>Docs</li>
+                </a>
+                <a href="/info" rel="noopener noreferrer">
+                    <li>Info</li>
+                </a>
+            </ul>
+            <LoginButton />
+        </nav>
+    );
 }
-
-function LoginButton(props: any) {
-    // Get the currently authenticated user
-    function getUser() {
-        const user  = supabase.auth.getUser();
-        return user;
-    }
-    const user = getUser();
-
-    if (user !== null) {
-        if (props.index) {
-            return (<a href="/login"><button className="mainLoginButton" style={{ background: "#4443" }}>Login</button></a>)
-        } else {
-            return (<a href="/login"><button className="mainLoginButton">Login</button></a>);
+async function getUser() {
+        const { data: session, error } = await supabase.auth.getSession();
+        if (error) {
+            console.error(error);
+            return null;
         }
+        console.log(session);
+        return null;
+    }
+function LoginButton() {
+    const [user, setUser] = useState<any>(null);
+
+    useEffect(() => {
+        setUser(getUser());
+
+    }, []);
+
+    function handleLoginClick() {
+        window.location.href = "/login";
+    }
+    if (user !== null) {
+        return (
+            <a href="/profile">
+                <button className="mainLoginButton">Profile</button>
+            </a>
+        )
     } else {
         return (
-            <a href="/profile"><button className="mainLoginButton">Profile</button></a>
+            <button className="mainLoginButton" onClick={handleLoginClick}>Login</button>
         )
     }
 }
