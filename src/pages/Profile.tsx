16494994d2abe0aef743ac03ba74234a4fe9f653
RoysManfo <manforoys@gmail.com>
2023-03-09 22:27:08 +0100

    Mmodified profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar';
 import { createClient } from '@supabase/supabase-js';
 import ENV from '../env';
 
+import "../css/profile/profile.css";
+
 export default function Profile() {
     // Create a single supabase client for interacting with your database
     const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SUPABASE_KEY);
@@ -27,8 +29,26 @@ export default function Profile() {
             <Navbar />
             <main>
                 <h1>Profile</h1>
+                <button className="mainLoginButton" onClick={logout}>Logout</button>
                 {/* <p>Email: { user }</p> */}
             </main>
         </>
     )
+    async function logout() {
+        // check if user is authenticated
+        const session = supabase.auth.getSession();
+        console.log(session);
+        if (!session) {
+            console.error("Cannot sign out, user is not authenticated.");
+            return;
+        }
+
+        // sign out user
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            console.error("Error signing out:", error);
+        } else {
+            console.log("User signed out.");
+        }
+    }
 }
