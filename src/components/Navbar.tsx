dd935267a8c518d16e7d5b9a19b6665fd1b403c5
RoysManfo <manforoys@gmail.com>
2023-03-09 23:20:48 +0100

    Remove unused imports

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import logo from "../img/logos/png/white-no-background.png";
 import { createClient } from '@supabase/supabase-js';
 import ENV from '../env';
