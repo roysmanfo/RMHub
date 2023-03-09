49bd4a434257ec7229af534341ea076a5011ca5a
RoysManfo <manforoys@gmail.com>
2023-03-09 23:43:24 +0100

    Conditional navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,28 @@ const supabase = createClient('https://roswbhnqbfckiuczsnrh.supabase.co', ENV.SU
 let loggedUser: any = null;
 
 export default function Navbar(props: any) {
+    if (props.index)
+        return (
+            <nav style={{ background: "transparent", opacity: 0, animation: "light 1s ease 4s forwards" }}>
+                <a href="/" className="logo" title="Torna alla pagina principale"><img src={logo} alt="Logo" style={{ width: "4rem" }} /></a>
+                <ul className="navList">
+                    <a href="/home" rel="noopener noreferrer">
+                        <li>Home</li>
+                    </a>
+                    <a href="/categories">
+                        <li>Categorie</li>
+                    </a>
+                    <a href="/unavaiable" rel="noopener noreferrer">
+                        <li>Docs</li>
+                    </a>
+                    <a href="/info" rel="noopener noreferrer">
+                        <li>Info</li>
+                    </a>
+                </ul>
+                <LoginButton />
+            </nav>
+        );
+
     return (
         <nav>
             <a href="/" className="logo" title="Torna alla pagina principale"><img src={logo} alt="Logo" style={{ width: "4rem" }} /></a>
@@ -72,3 +94,4 @@ function LoginButton() {
         )
     }
 }
+
