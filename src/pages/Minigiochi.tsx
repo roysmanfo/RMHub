48b1affc0b7a5095927e0adb99d40f546bcc64db
RoysManfo <manforoys@gmail.com>
2023-04-07 21:56:48 +0200

    Changed card image

--- src/pages/Minigiochi.tsx
+++ src/pages/Minigiochi.tsx
@@ -5,7 +5,7 @@ import Card from "../components/Card"
 import "../css/categories/categories.css"
 import "../css/minigiochi/minigiochi.css"
 
-import street from "../img/generic/bg-main_2.jpg"
+import street from "../img/generic/scissors3d.png"
 
 const CARDS = [
     {
@@ -22,14 +22,11 @@ export default function Minigiochi() {
         <>
             <Navbar />
             <main style={{ flexDirection: "column" }}>
-                <p className="font-9 weight-8 txt-center" style={{ marginTop: "10px", width: "100%", textTransform : "uppercase" }}>Minigiochi</p>
+                <p className="font-9 weight-8 txt-center" style={{ marginTop: "10px", width: "100%", textTransform: "uppercase" }}>Minigiochi</p>
                 <section style={{ display: "flex", justifyContent: "space-around" }}>
                     <Card caption={CARDS[0].caption} img={CARDS[0].img} link={CARDS[0].link} id={CARDS[0].id} />
                 </section>
             </main>
-
-
-
         </>
     )
 }
