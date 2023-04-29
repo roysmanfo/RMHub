161d0b5befc00f0b44d28449fd60245476d51dd2
RoysManfo <manforoys@gmail.com>
2023-04-30 01:19:44 +0200

    Fixed logic

--- src/pages/SCF.tsx
+++ src/pages/SCF.tsx
@@ -1,2 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import '../css/minigiochi/SCF.css'
 
+export default function SCF() {
+
+    const [games, setGames] = useState(0);
+    const [wins, setWins] = useState(0);
+    const [losses, setLosses] = useState(0);
+    const [draws, setDraws] = useState(0);
+    const [score, setScore] = useState(0);
+    const [winRate, setWinRate] = useState(0);
+    const [computerChoice, setComputerChoice] = useState("");
+
+    const choices = ["sasso", "carta", "forbice"];
+
+    const handleChoice = (choice: string) => {
+        const computerIndex = Math.floor(Math.random() * 3);
+        const computerChoice = choices[computerIndex];
+
+        setComputerChoice(computerChoice);
+        setGames((games) => games + 1);
+
+        if (choice === "sasso") {
+            if (computerChoice === "carta") {
+                setLosses((losses) => losses + 1);
+                setScore((score) => Math.max(score - 10, 0));
+            } else if (computerChoice === "forbice") {
+                setWins((wins) => wins + 1);
+                setScore((score) => score + 20);
+            } else {
+                setDraws((draws) => draws + 1);
+            }
+        } else if (choice === "carta") {
+            if (computerChoice === "forbice") {
+                setLosses((losses) => losses + 1);
+                setScore((score) => Math.max(score - 10, 0));
+            } else if (computerChoice === "sasso") {
+                setWins((wins) => wins + 1);
+                setScore((score) => score + 20);
+            } else {
+                setDraws((draws) => draws + 1);
+            }
+        } else if (choice === "forbice") {
+            if (computerChoice === "sasso") {
+                setLosses((losses) => losses + 1);
+                setScore((score) => Math.max(score - 10, 0));
+            } else if (computerChoice === "carta") {
+                setWins((wins) => wins + 1);
+                setScore((score) => score + 20);
+            } else {
+                setDraws((draws) => draws + 1);
+            }
+        }
+
+        setWinRate(parseFloat((wins / games).toFixed(2)));
+    };
+
+    return (
+        <>  
+            <h1 style={{ color: '#fff', fontSize: '5rem' }}>SASSO CARTA FORBICE</h1>
+            <div className="game">
+                <div>
+                    <h2 id="SceltaComputer">Il computer ha scelto {computerChoice || "nulla"}</h2>
+                    <div className="scelte">
+                        <button className="sasso scelta" id="sasso" onClick={() => handleChoice("sasso")}>SASSO</button>
+                        <button className="carta scelta" id="carta" onClick={() => handleChoice("carta")}>CARTA</button>
+                        <button className="forbice scelta" id="forbice" onClick={() => handleChoice("forbice")}>FORBICE</button>
+                    </div>
+                </div>
+                <div>
+                    <div className="stats">
+                        <p id="gamesPlayed">Partite: 0</p>
+                        <p id="Score">Punteggio: 0</p>
+                        <p id="Wins">Vittorie: 0</p>
+                        <p id="Loses">Sconfitte: 0</p>
+                        <p id="Draws">Pareggi: 0</p>
+                        <p id="WinRate">Win rate: 0</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
