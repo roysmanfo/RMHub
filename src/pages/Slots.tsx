d1cc353b5800c1242127274f71d56dcf36d64021
RoysManfo <manforoys@gmail.com>
2023-05-09 22:49:21 +0200

    Modify UI

--- src/pages/Slots.tsx
+++ src/pages/Slots.tsx
@@ -2,23 +2,35 @@ import React from "react";
 import "../css/minigiochi/slots.css";
 
 export default function Slots() {
-    document.querySelector("body")?.classList.add("Slots");
-
-
     return (
         <>
-            <main>
-                <h1>Slots</h1>
+            <main className="Slots">
                 <div className="game">
                     <aside>
-                        <p>Stats</p>
+                        <div className="slot-records">
+                        <Record />
+                        <Record />
+                        <Record />
+                        </div>
                     </aside>
                     <section>
-                        <div>SLOTS</div>
-                        <div>BUTTONS</div>
+                        <div className="slots"></div>
+                        <div className="playbar"></div>
                     </section>
                 </div>
             </main>
         </>
+    )
+}
+
+
+function Record(){
+    let amount = 500;
+    return (
+        <div className="record">
+            <div className="icon"></div>
+            <h5 style={{color: "#fff", margin: "0"}}>Player1</h5>
+            <p style={{ fontSize: '1rem',fontWeight: 900 , margin: '10px 0' }}>${amount}</p>
+        </div>
     )
 }
