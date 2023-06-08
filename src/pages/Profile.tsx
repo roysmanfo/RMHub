08ddfa0707d63eaff01320994384873772d73385
RoysManfo <manforoys@gmail.com>
2023-06-08 19:48:33 +0200

    redirect user if not logged in

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -11,21 +11,27 @@ export default function Profile() {
     const [userData, setUserData] = useState<{[x: string]: any}>({});
     
     // setUserData({id: undefined, username: '', biography: '' });
-
+    
     useEffect(() => {
         async function fetchUser() {
-            if(user === null){
-                const { data: { user } } = await supabase.auth.getUser();
-                setUser(user ?? null);
-                if (user !== null){
-                    let { data: userData, error } = await supabase.from('users').select('*').eq('id', user?.id);
-                    if (userData){                                                
-                        if(error || userData[0] === undefined){                            
-                            setUserData(createUserData(user));
-                                                      
-                        }
-                        setUserData(userData[0]);
+            const session = await supabase.auth.getSession();
+            
+            // Check if the user is not logged in
+            if(!session.data.session) 
+                window.location.href = '/login';
+                
+            // If the user is logged in
+            if(session.data.session){
+                const user = session.data.session.user;
+                setUser(user);
+                
+                let { data: userData, error } = await supabase.from('users').select('*').eq('id', user?.id);
+                if (userData){                                                
+                    if(error || userData[0] === undefined){                            
+                        setUserData(createUserData(user));
+                                                    
                     }
+                    setUserData(userData[0]);
                 }
             }
         }
