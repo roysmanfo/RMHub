ee1305d3d52e50695aef09be5381918ff65a0a9b
RoysManfo <manforoys@gmail.com>
2023-09-04 18:50:08 +0200

    configuration

---
+++ src/conf/supabase.tsx
@@ -0,0 +1,47 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL || "",
+    process.env.REACT_APP_SUPABASE_KEY || ""
+);
+
+
+export async function fetchUser() {
+    try {
+        const {data, error} = await supabase.auth.getUser();
+        if (data)
+            return data.user;
+        
+        if (error)
+            throw error;
+
+    } catch (error: any) {
+        console.log(error.msg);
+    }
+}
+
+export async function fetchSession() {
+    try {
+        const {data, error} = await supabase.auth.getSession();
+        if (data)
+            return data.session;
+        
+        if (error)
+            throw error;
+
+    } catch (error: any) {
+        console.log(error.message);
+    }
+}
+
+
+
+
+
+
+export default supabase;
+export const user = await fetchUser();
+export const session = await fetchSession();
+
+
